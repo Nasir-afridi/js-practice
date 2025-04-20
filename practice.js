723b973dbcc78bce69daa802767c8fe29c65262f
Nasir-afridi <nasirwc05@gmail.com>
2025-04-20 12:14:07 +0500

    problem-4 reversed the number

--- practice.js
+++ practice.js
@@ -6,26 +6,30 @@
 
 
                     02.
-let numbers = [4, 10, 2, 99, 23];
-let max = numbers[0];
-for(let i = 1; i < numbers.length; i++) {
-    if(numbers[i] > max) {
-        max = numbers[i];
-    }
-}
-console.log(max)
-*/
+    let numbers = [4, 10, 2, 99, 23];
+    let max = numbers[0];
+    for(let i = 1; i < numbers.length; i++) {
+        if(numbers[i] > max) {
+            max = numbers[i];
+        }
+    }console.log(max)
 
-let number = 90;
-let isPrime = true;
 
-for(let i = 2; i < number; i++) {
-    if(number % i === 0) {
-        isPrime = false;
-        break;
-    }}
-    if(isPrime && number > 1) {
-        console.log('Prime');
-    } else{
-        console.log('Not prime')
-    }
+                    03.
+    let number = 90;
+    let isPrime = true;
+    for(let i = 2; i < number; i++) {
+        if(number % i === 0) {
+            isPrime = false;
+            break;
+        }}
+        if(isPrime && number > 1) {
+            console.log('Prime');
+        } else{
+            console.log('Not prime')
+        }
+    */
+
+    let number = 123456;
+    let reverse = number.toString().split('').reverse().join('');
+    console.log(reverse)
