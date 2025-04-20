cfaeb18d4fa41012b89068a9c22897480bcb0aa8
Nasir-afridi <nasirwc05@gmail.com>
2025-04-20 12:05:33 +0500

    problem-3 chk the number is prime or not

--- practice.js
+++ practice.js
@@ -1,10 +1,11 @@
 /*
+                    01.
     let string = 'Nasir';
     let reverse = string.split('').reverse('').join('');
     console.log(reverse);
-*/
 
 
+                    02.
 let numbers = [4, 10, 2, 99, 23];
 let max = numbers[0];
 for(let i = 1; i < numbers.length; i++) {
@@ -13,3 +14,18 @@ for(let i = 1; i < numbers.length; i++) {
     }
 }
 console.log(max)
+*/
+
+let number = 90;
+let isPrime = true;
+
+for(let i = 2; i < number; i++) {
+    if(number % i === 0) {
+        isPrime = false;
+        break;
+    }}
+    if(isPrime && number > 1) {
+        console.log('Prime');
+    } else{
+        console.log('Not prime')
+    }
