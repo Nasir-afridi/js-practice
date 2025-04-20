603a3230e5f22dc094e8266cd1e1ebacc4bbda67
Nasir-afridi <nasirwc05@gmail.com>
2025-04-20 11:54:14 +0500

    problem-2 max number in array

--- practice.js
+++ practice.js
@@ -1,3 +1,15 @@
-let string = 'Nasir';
-let reverse = string.split('').reverse('').join('');
-console.log(reverse);
+/*
+    let string = 'Nasir';
+    let reverse = string.split('').reverse('').join('');
+    console.log(reverse);
+*/
+
+
+let numbers = [4, 10, 2, 99, 23];
+let max = numbers[0];
+for(let i = 1; i < numbers.length; i++) {
+    if(numbers[i] > max) {
+        max = numbers[i];
+    }
+}
+console.log(max)
