3e9fbf0209f8a41da1baee4175a251c8deeee4c9
Nasir-afridi <nasirwc05@gmail.com>
2025-04-23 12:13:45 +0500

    problem-8 fizzbuzz added

--- practice.js
+++ practice.js
@@ -57,13 +57,26 @@
                     07.
     let string = 'madam';
     let reverse = string.split('').reverse().join('');
-
-
     if(reverse === string){
         console.log('palindrome');
     }else{
         console.log('not palindrome')
     }
+        
+   
+                    08.
+    for(let i = 1; i <= 100; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log(i + '-FizzBuzz');
+        }
+        else if(i % 3 === 0) {
+            console.log(i + '-Fizz');
+        }
+       else if(i % 5 === 0) {
+            console.log(i + '-Buzz');
+        }
+        else{
+            console.log(i);
+        }
+    }
     */
-
-    
