3f132697b8208f85a5b395c56d2276b88dd4ae3c
Nasir-afridi <nasirwc05@gmail.com>
2025-04-23 17:30:44 +0500

    breaking the counter and button into component

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,24 +1,13 @@
-import './App.css';
 import React, { useState } from 'react';
+import Button from './button';
+import Counter from './counter';
+import './App.css'
 
 function App() {
-  const [a, setA] = useState(0);
-
-  function increased() {
-    setA(a+1)
-  }
-  function decreased() {
-    setA(a-1)
-  }
-  if(a < 0) {
-    alert('Number less than zero are not allowed');
-    setA(0);
-  }
   return (
     <>
-    <h3>Number is {a}</h3>
-      <button onClick={increased}>Increased</button>
-      <button onClick={decreased}>Decreased</button>
+      <Counter></Counter>
+      <Button></Button>
     </>
   )
 }
