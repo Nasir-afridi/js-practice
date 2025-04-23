a2cf918b0046106df7e7c12f801ebad4918cf914
Nasir-afridi <nasirwc05@gmail.com>
2025-04-23 17:31:33 +0500

    Counter break down into component

---
+++ React/src/counter.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+
+const counter = () => {
+  const [a, setA] = useState(0);
+    function increased() {
+      setA(a+1)
+    }
+    function decreased() {
+      setA(a-1)
+    }
+    function reset() {
+        setA(0)
+    }
+    if(a < 0) {
+      alert('Number less than zero are not allowed');
+      setA(0);
+    }
+    return (
+    <>
+      <h3>Number is {a}</h3>
+      <div className=''>
+        <button className='buttons' onClick={increased}>Increased</button>
+        <button className='buttons' onClick={decreased}>Decreased</button>
+        <button className='buttons' onClick={reset}>Reset</button>
+      </div>
+      
+
+    </>
+  )
+}
+
+export default counter
