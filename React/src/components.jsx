7dbc3a5a76dbe3fb7087b249799a468559b69100
Nasir-afridi <nasirwc05@gmail.com>
2025-04-25 00:39:11 +0500

    added the card component and passing the value through props

---
+++ React/src/components.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './components/Card';
+
+const Components = () => {
+  return (
+    <>
+      <Card a = 'Nasir'></Card>
+    </>
+  );
+};
+
+export default Components;
