46eed87f3519899d873d1398436e19b1cbe4b00b
Nasir-afridi <nasirwc05@gmail.com>
2025-04-24 15:50:43 +0500

    form handling done with the help of preventDefault

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,10 +4,19 @@ import Counter from './counter';
 import './App.css'
 
 function App() {
+  function submit(q) {
+   q.preventDefault();
+    console.log('first');
+  }
   return (
     <>
       <Counter></Counter>
       <Button></Button>
+      <form onSubmit={(q)=>{
+        submit(q)
+      }}><input type="text" />
+      <button type="submit">submit</button></form>
+      
     </>
   )
 }
