193e1903dd5a2c6f28457260fa033304d1c3f278
Nasir-afridi <nasirwc05@gmail.com>
2025-04-24 16:59:35 +0500

    changes added

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -7,9 +7,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <Counter></Counter>
-      <Button></Button>
-      <Input></Input>
+      <Counter />
+      <Button />
+      <Input />
     </>
   )
 }
