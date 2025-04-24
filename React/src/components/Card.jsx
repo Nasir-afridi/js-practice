d2fa3c388c9579d3d8265f792c167672771265ff
Nasir-afridi <nasirwc05@gmail.com>
2025-04-25 01:39:03 +0500

    received the props data

--- React/src/components/Card.jsx
+++ React/src/components/Card.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Card = (hello) => {
+const Card = (props) => {
   return (
-    <div>UserName is : {hello.a}</div>
+    <>
+        <div className="w-50 bg-primary text-light inline-block p-5 text-center rounded mb-5">
+            <h1 className='mb-4'>{props.name}</h1>
+            <h2>{props.city},, {props.age},, {props.profession}</h2>
+            <button className='bg-danger mt-5 btn btn-lg text-danger-emphasis'>Add friend</button>
+        </div>
+    </>
   )
 }
 
