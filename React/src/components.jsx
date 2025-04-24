71e6ab7d56785982f325b5433eca396b9feaf1b0
Nasir-afridi <nasirwc05@gmail.com>
2025-04-25 01:38:29 +0500

    passing data with the help of props using map

--- React/src/components.jsx
+++ React/src/components.jsx
@@ -2,10 +2,45 @@ import React from 'react';
 import Card from './components/Card';
 
 const Components = () => {
+    const users = [
+        {
+          "username": "ali_123",
+          "profession": "Frontend Developer",
+          "age": 24,
+          "city": "Lahore"
+        },
+        {
+          "username": "sana_k",
+          "profession": "Graphic Designer",
+          "age": 27,
+          "city": "Karachi"
+        },
+        {
+          "username": "hamza_dev",
+          "profession": "Backend Developer",
+          "age": 30,
+          "city": "Islamabad"
+        },
+        {
+          "username": "ayesha_writer",
+          "profession": "Content Writer",
+          "age": 22,
+          "city": "Rawalpindi"
+        },
+        {
+          "username": "usman_ai",
+          "profession": "AI Engineer",
+          "age": 28,
+          "city": "Faisalabad"
+        }
+      ]
+      
   return (
-    <>
-      <Card a = 'Nasir'></Card>
-    </>
+    <div className='p-5'>
+      {users.map(function(elem) {
+        return <Card name = {elem.username} profession={elem.profession} age={elem.age} city={elem.city} />
+      })}
+    </div>
   );
 };
 
